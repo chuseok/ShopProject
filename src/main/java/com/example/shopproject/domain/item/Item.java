cb3c26696e84cd4a40a8f7a2c3860ab52a6104cd
chuseok <90127lucy@gmail.com>
2023-06-28 17:28:09 +0900

    상품 도메인 개발

--- src/main/java/com/example/shopproject/domain/item/Item.java
+++ src/main/java/com/example/shopproject/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.example.shopproject.domain.item;
 
 import com.example.shopproject.domain.Category;
+import com.example.shopproject.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -23,4 +24,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
