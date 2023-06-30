bd23993e8f7cb5622291b08adf73a31f8b97e52c
chuseok <90127lucy@gmail.com>
2023-06-30 17:55:56 +0900

    OrderService TestCode 추가

--- src/main/java/com/example/shopproject/service/OrderService.java
+++ src/main/java/com/example/shopproject/service/OrderService.java
@@ -12,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -43,7 +45,17 @@ public class OrderService {
         return order.getId();
     }
 
-    //취소
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        Order order = orderRepository.findOne(orderId);
+        order.cancel();
+    }
 
     //검색
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//         return orderRepository.findAll(orderSearch);
+//    }
 }
